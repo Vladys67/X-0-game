3bb993c619b2300686a1b595f317d488ffceaeb7
Vladys67 <vladislav2005.10.06@gmail.com>
2025-09-29 12:40:31 +0300

    Adaugare implementare X-0 cu fisiere cpp si Makefile in lab02

--- engine.cpp
+++ engine.cpp
@@ -1,10 +1,11 @@
 #include "Engine.hpp"
 
-Engine::Game(const Player& p1, const Player& p2)
+Engine::Engine(const Player& p1, const Player& p2)
     : _player1(p1), _player2(p2), _currentTurn(p1.GetSymbol()) {}
 
-void Engine::Start()
-bool Engine::MakeMove(int row, int col) { return true; } 
+void Engine::Start() {}
+
+bool Engine::MakeMove(int row, int col) { return true; }
 bool Engine::CheckWin(Symbol symbol) const { return false; }
 bool Engine::IsDraw() const { return false; }
 void Engine::SwitchTurn() {}
