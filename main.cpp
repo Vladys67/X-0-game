ded6d201e9f0be3a90e96092350d960bc06a0d26
Vladys67 <vladislav2005.10.06@gmail.com>
2025-09-29 12:50:26 +0300

    Adaugare implementare X-0 cu fisiere cpp si Makefile in lab02

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "engine.hpp"
+#include "player.hpp"
+#include "ui.hpp"
 
 int main() {
-    GameEngine engine;
-    engine.Init();
-    engine.Run();
+    Player p1(Symbol::X); 
+    Player p2(Symbol::O); 
+    Engine engine(p1, p2); 
+    engine.Start(); 
     return 0;
 }
