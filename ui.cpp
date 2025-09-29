51f57a608f72ec2ab75a4dbf8585d34c992c6831
Vladys67 <vladislav2005.10.06@gmail.com>
2025-09-29 12:44:56 +0300

    Adaugare implementare X-0 cu fisiere cpp si Makefile in lab02

--- ui.cpp
+++ ui.cpp
@@ -1,4 +1,10 @@
 #include "UI.hpp"
+#include <string>
+#include <iostream>
 
-void UI::ShowBoard(const Board& board) 
-void UI::ShowMessage(const std::string& msg) 
+void UI::ShowBoard(const Board& board) {
+}
+
+void UI::ShowMessage(const std::string& msg) {
+    std::cout << msg << std::endl;
+}
